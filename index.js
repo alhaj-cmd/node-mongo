c0455a40cd5a4355280eca29200d34ad2a530c9e
Your Namealhaj-cmd <alhazsarkar5523@gmail.com>
2021-04-12 23:15:31 +0600

    my fisrt node project

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 
 const users = [ 'suho','sujon','ali','suni','sumi']
 
@@ -14,10 +20,19 @@ app.get('/', (req,res) =>{
     res.send(fruit);
 
 })
+//get
 app.get('/users/:id',(req,res) => {
     const id = req.params.id;
     const name = users[id];
     res.send({id,name});
 })
+//post
+app.post('/addUser', (req, res) => {
+   // console.log('data recipt', req.body);
+   // save to data base
+   const user = req.body;
+   user.id = 55;
+   res.send(user);
+ })
 
 app.listen(3000, () => console.log('listenting to port 3000'));
